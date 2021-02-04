49e1f9c3f222b6432cd6ad862738805f6141fe6b
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-02-04 21:23:51 +0000

    Add and deploy basic question API

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,5 +1,3 @@
-import * as functions from "firebase-functions";
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,3 +5,28 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+import "source-map-support/register";
+import {https} from "firebase-functions";
+import express from "express";
+// import cors from 'cors';
+import {v4 as uuid} from "uuid";
+
+const app = express();
+
+// app.use(authMiddleware)
+
+app.get("/:id/question", (request, response) => {
+  // Sanitize input
+
+  // Retrieve the current question for the quiz.
+  // let quizId = request.params.id // Use this
+
+  response.send({
+    id: uuid(),
+    text: "How Evil Is Your Cat?",
+    answerOptionType: "MultipleChoiceTextAnswerOption",
+    answerOption: ["Very evil", "Covid 19 evil", "Cute"],
+  });
+});
+
+exports.quiz = https.onRequest(app);
